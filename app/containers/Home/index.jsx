a661c2855880edb04b961d35d006be96abcff0d2
GanYihuan <583520052@qq.com>
2018-01-19 14:59:15 +0800

    React Performance Optimization

--- app/containers/Home/index.jsx
+++ app/containers/Home/index.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+// 性能检测
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
@@ -12,6 +13,10 @@ import List from './subpage/List';
 class Home extends Component {
   constructor(props) {
     super(props);
+    // 一些无效的改动触发了shouldComponentUpdate函数，也会导致无效的更新
+    // 重写组件的shouldComponentUpdate函数，在每次更新之前判断props和state，
+    // 如果有变化则返回true，无变化则返回false。
+    // 因此，我们在开发过程中，在每个 React 组件中都尽量使用PureRenderMixin
     this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
   }
 
