bcce27a0ef6b5b718d49720a0509321489dacf2c
GanYihuan <583520052@qq.com>
2018-01-30 15:22:45 +0800

    HomeHeader format

--- docs/test.js
+++ docs/test.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import {Link} from 'react-router-dom';
-import SearchInput from '../SearchInput';
-import styles from './style.less';
+import SearchInput from '';
+import styles from '';
 
 
 class HomeHeader extends Component {
@@ -16,30 +16,7 @@ class HomeHeader extends Component {
 
   render() {
     return (
-        <div id={styles['home-header']} className={"clear-fix"}>
-          <div className={styles['home-header-left'] + ' float-left'}>
-            <Link to={'/city'}>
-              <span>{this.props.cityName}</span>
-              &nbsp;
-              <i className={'icon-angle-down'}/>
-            </Link>
-          </div>
-          <div className={styles['home-header-right'] + 'float-right'}>
-            <Link to={'/login'}>
-              <i className={'icon-user'}/>
-            </Link>
-          </div>
-          <SearchInput value={''} enterHandle={this.enterHandle.bind(this)}/>
-        </div>
+        <div id={styles['home-header']} className={'clear-fix'}></div>
     )
   }
-
-  enterHandle(value) {
-    // js方式路由
-    // 跳转的到指定的路由
-    // all 类型
-    this.props.history.push('/search/all/' + encodeURIComponent(value));
-  }
 }
-
-export default HomeHeader;
