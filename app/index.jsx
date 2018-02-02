a7fdae9d2073426971672bff80a10a8b5bb50979
GanYihuan <583520052@qq.com>
2018-02-03 00:06:07 +0800

    format .

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
-// redux.
+// redux
 import {Provider} from 'react-redux';
 import configureStore from './store/configureStore';
 // react-router.
